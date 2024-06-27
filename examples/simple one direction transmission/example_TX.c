a05f5a47db87b0fcf7aa21d15e8f4d63199cd97e
Guillem <guillem.prenafeta@gmail.com>
2024-06-27 12:04:31 +0200

    .

--- examples/simple one direction transmission/example_TX.c
+++ examples/simple one direction transmission/example_TX.c
@@ -1,281 +1,224 @@
-/* nRF24L01 library by Guillem Prenafeta */
-
-/* SPI library (Library provided by the user micrcontroller). Important functions
-*
-*	SPI_send (uint8_t count, uint8t *TxBuf, uint8_t *RxBuf);
+/* 
+ *  One direction transmission example (master TX)
+ *	by Guillem Prenafeta (UB 2024)
+ *
 */
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
 #include "nRF24L01lib.h"
 
-#define RF_CH 0x07 // RF Chanel, to choose by the user
+//--- DRIVERLIB libraries only for TIVA-C ---//
+#include "inc/hw_memmap.h"
+#include <inc/hw_gpio.h>
+#include <inc/hw_types.h>
+#include "driverlib/sysctl.h" //libreria del sistema
+#include "driverlib/gpio.h"
+#include "driverlib/pwm.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/timer.h"
+#include "driverlib/interrupt.h"
+#include "inc/hw_ints.h"
+#include "driverlib/uart.h"
+#include "inc/hw_types.h"
+#include "inc/hw_gpio.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/ssi.h"
+#include "inc/hw_ssi.h"
+// ------------------------------------------//
+
+// Functions to be defined::  //TODO depends on your HW and abstraction lvl
+/*
+ * void SPI_send (uint8_t count, uint8t *TxBuf, uint8_t *RxBuf);  
+ * void nRF24L01_CSN(bool state);
+ * void nRF24L01_CE(bool state);
+ * void delay100us(uint32_t ticks);
+ *
+ * In this example TIVA-C will be used. Using TIVAWARE library to programm the functions
+*/
+
+#define RF_CH 0x07 // RF Chanel, to choose by the user (freq CH 2400 MHz + RF_CH (7) == 2407 MHz)
+
+// HW functions to init clocks, and init 3-wire SPI and GPIOs, 2 output and 1 input
+void init_HW(void);
+void ISRtimerDelay(void);
+
+// HW functions to controll nRF24
+void SPI_send (uint8_t count, uint8t *TxBuf, uint8_t *RxBuf);
+void nRF24L01_CSN(bool state);
+void nRF24L01_CE(bool state);
+void delay100us(uint32_t ticks);
+
+bool RADIOint;
+uint32_t payloadRXLEN;
+uint8_t payloadRX[32];
+
+void main()
+{
+
+	init_HW(); //init_all
+
+	nRF24L01_CE(false); //set CE to disable
+	nRF24L01_CSN(true); //SPI disable
+
+	RADIOint = false;
+
+
+	while(1)
+	{
+
+		//missatge
+		char array[] = "HOLA MUNDO"
+		nRF24L01_write_tx_payload_PTX((uint8_t*)array, sizeof(array), true); //enviem
+		while(RADIOint==false){ //esperem int
+		}
+		RADIOint=false;
+		uint8_t status = nRF24L01_nop();
+		if((status&nrf24l01_STATUS_TX_DS)==nrf24l01_STATUS_TX_DS){ //si es que s'ha enviat correctament
+			//llegim missatge
+			do{
+                nRF24L01_payload_width(&payloadRXLEN);
+                nRF24L01_read_rx_payload(payloadRX, payloadRXLEN); //i fem processament
+            }while(!nRF24L01_FIFO_RX_empty()); //mentres estigui plena (quan buida passa a true)
+            //ja tenim les dades
+    	} else if((status&nrf24l01_STATUS_MAX_RT)==nrf24l01_STATUS_MAX_RT){//error de transmissio s'haura de torna a enviar
+    		__nop();
+    	}
+
+
+		delay100us(100); //10ms
+	}
+}
+
+
 
 //-----------------------------------------------------------------------------------------------------------//
-void nRF24L01_init(void)
-{
-
-	uint8_t data;
-
-	data=nrf24l01_EN_AA_ENAA_P0;
-	nRF24L01_writeReg(nrf24l01_EN_AA, &data, 1); //configurem pipe 0 de auto AA
-
-	data=0;
-	nRF24L01_readReg(nrf24l01_EN_AA, &data, 1); //configurem pipe 0 de auto AA
-
-#if defined(UARTdebug)
-	if(data==nrf24l01_EN_AA_ENAA_P0){
-	    write("nRF24 configurada correctament", true);
-	} else{
-	    write("Error configuracio nRF24", true);
-	}
-#endif
-
-	data=nrf24l01_EN_RXADDR_ERX_P0;
-	nRF24L01_writeReg(nrf24l01_EN_RXADDR, &data, 1); //configurem adress recepcio PIPE0
-
-	data=0x01;
-	nRF24L01_writeReg(nrf24l01_SETUP_AW, &data, 1); //configurem adress de 3 Bytes
-
-	data=(0x03<<4) | 0x08;
-	nRF24L01_writeReg(nrf24l01_SETUP_RETR, &data, 1); //configurem retransmissio, amb 1000us d'espera i 8 intents
-
-	data=RF_CH;
-	nRF24L01_writeReg(nrf24l01_RF_CH, &data, 1); //configurem frequency chanel
-
-	data=(0x10<<1);
-	nRF24L01_writeReg(nrf24l01_RF_SETUP, &data, 1); //configurem air-dataRate de 1Mbps i output power de -6dBm
-
-	data=nrf24l01_STATUS_RX_DR|nrf24l01_STATUS_TX_DS|nrf24l01_STATUS_MAX_RT;
-	nRF24L01_writeReg(nrf24l01_STATUS, &data, 1); //netejem interrupcions per si de cas
-
-	//nRF24L01_writeReg(nrf24l01_RX_ADDR_P0, RX_ADDR_P0, N); //si volem canviar direccio del pipeline //ojo LSB first i s'han d'enviar tants bytes comm configurats al SETUP_AW
-
-	//nRF24L01_writeReg(nrf24l01_TX_ADDR, TX_ADDR, N); //configurem PTX adress que envia, si fem servir ShockBurst dona igual (S'ha de posar RX_ADDR_P0) //ojo LSB first i s'han d'enviar tants bytes comm configurats al SETUP_AW
-
-	//nRF24L01_writeReg(nrf24l01_RX_PW_P0, N, 1);  //quan vulguem posar payload de RX haurem de configurar aquest registre (R/W)
-
-	data=(0x01<<1)|(0x01<<2);
-	nRF24L01_writeReg(nrf24l01_FEATURE, &data , 1);  //enable DYNPD i Payload ACK
-
-	//proves mirem que hem escrit algo be
-	data=0;
-	nRF24L01_readReg(nrf24l01_FEATURE, &data , 1);
-
-
-	data=0x01;
-	nRF24L01_writeReg(nrf24l01_DYNPD, &data, 1);  //activem dynamic payload del pipe 0
-
-	nRF24L01_flush_tx();
-	nRF24L01_flush_rx(); //fem un flush
-}
-
-void nRF24L01_powerUP(uint8_t mode)
-{
-	uint8_t data;
-	if(mode==PTXmode){
-//		data=nrf24l01_CONFIG_MASK_TX_DS|nrf24l01_CONFIG_MASK_MAX_RT|(0x01<<3)|(0x01<<2)|(0x01<<1)|(0x00);  //EN_CRC, CRC 2 Bytes, POWER_UP, TX
-	    data=nrf24l01_CONFIG_MASK_RX_DR|(0x01<<3)|(0x01<<2)|(0x01<<1)|(0x00);  //EN_CRC, CRC 2 Bytes, POWER_UP, TX
-	} else if(mode==RTXmode){
-//		data=nrf24l01_CONFIG_MASK_RX_DR|(0x01<<3)|(0x01<<2)|(0x01<<1)|(0x01);  //EN_CRC, CRC 2 Bytes, POWER_UP, RX
-	    data=nrf24l01_CONFIG_MASK_TX_DS|nrf24l01_CONFIG_MASK_MAX_RT|(0x01<<3)|(0x01<<2)|(0x01<<1)|(0x01);  //EN_CRC, CRC 2 Bytes, POWER_UP, RX
-	} else{
-		while(1); //error
-	}
-	nRF24L01_writeReg(nrf24l01_CONFIG, &data, 1);
-}
-
-void nRF24L01_powerDOWN(void)
-{
-	uint8_t data;
-	nRF24L01_readReg(nrf24l01_CONFIG, &data, 1); //llegim conf actual
-
-	if((data & nrf24l01_CONFIG_PWR_UP) == 0)
-		return;
-
-	data &= ~nrf24l01_CONFIG_PWR_UP;
-
-	nRF24L01_writeReg(nrf24l01_CONFIG, &data, 1);
-}
-
-//-----------------------------------------------------------------------------------------------------------//
-uint8_t nRF24L01_writeReg(uint8_t regnumber, uint8_t * data, uint8_t len)
-{
-	uint8_t status;
-	uint8_t dataR;
-	dataR=regnumber | nrf24l01_W_REGISTER; //volem escriure
-	//baixem CS
-	nRF24L01_CSN(false);
-	SPI_send(1, &dataR, &status);
-	//ara enviem les dades
-	SPI_send(len, data, NULL);
-	//pujem CS
-	nRF24L01_CSN(true);
-
-	return status;
-}
-
-uint8_t nRF24L01_readReg(uint8_t regnumber, uint8_t * data, uint8_t len){
-	uint8_t status;
-	//baixem CS
-	nRF24L01_CSN(false);
-	SPI_send(1, &regnumber, &status); //escribim
-	//ara enviem les dades
-	SPI_send(len, NULL, data); //llegim
-	//pujem CS
-	nRF24L01_CSN(true);
-
-	return status;
-}
-uint8_t nRF24L01_write_tx_payload_PTX(uint8_t * data, uint8_t len, bool transmit)
-{
-	uint8_t status, dataR;
-	dataR=nrf24l01_W_TX_PAYLOAD;
-	//baixem CS
-	nRF24L01_CSN(false);
-	SPI_send(1, &dataR, &status); //escribim
-	//ara enviem les dades
-	SPI_send(len, data, NULL);
-	//pujem CS
-	nRF24L01_CSN(true);
-
-	if(transmit){
-		nRF24L01_transmit();
-	}
-
-	return status;
-}
-uint8_t nRF24L01_read_rx_payload(uint8_t * data, uint8_t len)
-{
-	uint8_t status, dataR;
-	dataR=nrf24l01_R_RX_PAYLOAD;
-	//baixem CS
-	nRF24L01_CSN(false);
-	SPI_send(1, &dataR, &status); //escribim
-	//ara enviem les dades
-	SPI_send(len, NULL, data);
-	//pujem CS
-	nRF24L01_CSN(true);
-
-	return status;
-}
-uint8_t nRF24L01_flush_tx(void)
-{
-	uint8_t status,data;
-	data=nrf24l01_FLUSH_TX;
-	//baixem CS
-	nRF24L01_CSN(false);
-	SPI_send(1, &data, &status); //escribim
-	nRF24L01_CSN(true);
-
-	return status;
-}
-uint8_t nRF24L01_flush_rx(void)
-{
-	uint8_t status, data;
-	data=nrf24l01_FLUSH_RX;
-	//baixem CS
-	nRF24L01_CSN(false);
-	SPI_send(1, &data, &status); //escribim
-	nRF24L01_CSN(true);
-
-	return status;
-}
-uint8_t nRF24L01_reuse_tx_pl(void)
-{	
-	uint8_t status, data;
-	data=nrf24l01_REUSE_TX_PL; //comandes SPI
-	//baixem CS
-	nRF24L01_CSN(false);
-	SPI_send(1, &data, &status); //escribim
-	nRF24L01_CSN(true);
-
-	return status;
-}
-uint8_t nRF24L01_payload_width(uint8_t *data)
-{
-	uint8_t status, dataR;
-	dataR=0x60; //comandes SPI, llegir payload width
-	//baixem CS
-	nRF24L01_CSN(false);
-	SPI_send(1, &dataR, &status); //escribim
-	SPI_send(1, NULL, data); //llegim length
-	nRF24L01_CSN(true);
-
-	return status;
-}
-uint8_t nRF24L01_write_tx_payload_PRX(uint8_t * data, uint8_t len, uint8_t pipeline)
-{
-	uint8_t status, dataR;
-	dataR=pipeline | 0xA8;
-	//baixem CS
-	nRF24L01_CSN(false);
-	SPI_send(1, &dataR, &status); //escribim
-	//ara enviem les dades
-	SPI_send(len, data, NULL);
-	//pujem CS
-	nRF24L01_CSN(true);
-
-	return status;
-}
-uint8_t nRF24L01_nop(void)
-{
-	uint8_t status, data;
-	data=nrf24l01_NOP; //comandes SPI
-	//baixem CS
-	nRF24L01_CSN(false);
-	SPI_send(1, &data, &status); //escribim
-	nRF24L01_CSN(true);
-
-	return status;
-}
-
-void nRF24L01_transmit(void)
-{
-	nRF24L01_CE(true);
-	delay100us(1); //esperem >10us (100us)
-	nRF24L01_CE(false);
-}
-
-bool nRF24L01_FIFO_RX_empty(void){
-    uint8_t data;
-    nRF24L01_readReg(nrf24l01_FIFO_STATUS, &data, 1);
-    if((data&0x01)==0x01){
-        return true;
-    } else{
-        return false;
-    }
-}
-bool nRF24L01_FIFO_RX_full(void){
-    uint8_t data;
-    nRF24L01_readReg(nrf24l01_FIFO_STATUS, &data, 1);
-    if((data&0x02)==0x02){
-        return true;
-    } else{
-        return false;
-    }
-}
-bool nRF24L01_FIFO_TX_empty(void){
-    uint8_t data;
-    nRF24L01_readReg(nrf24l01_FIFO_STATUS, &data, 1);
-    if((data&0x10)==0x10){
-        return true;
-    } else{
-        return false;
-    }
-}
-bool nRF24L01_FIFO_TX_full(void){
-    uint8_t data;
-    nRF24L01_readReg(nrf24l01_FIFO_STATUS, &data, 1);
-    if((data&0x20)==0x20){
-        return true;
-    } else{
-        return false;
-    }
-}
-
+//  FUNCTIONS
 //-----------------------------------------------------------------------------------------------------------//
 
-void nRF24L01_clear_IRQ(void)
+void nRF24ISR(void){
+
+	GPIOIntClear(GPIO_PORTC_BASE, GPIO_INT_PIN_5);
+    nRF24L01_CE(false);
+
+    RADIOint=true; //indiquem que ha saltat int
+}
+
+void init_HW(void)
 {
-	uint8_t data;
-	data=nrf24l01_STATUS_RX_DR|nrf24l01_STATUS_TX_DS|nrf24l01_STATUS_MAX_RT;
-	nRF24L01_writeReg(nrf24l01_STATUS, &data, 1);
+
+	SysCtlClockSet(
+            SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ); //50 MHz
+
+
+	//Timer for nRF24lib (period 100us)
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER1);
+    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER1))
+    {
+        __nop();
+    }
+    TimerClockSourceSet(TIMER1_BASE, TIMER_CLOCK_PIOSC); //16MHz on 100us==16000000*100*10**-6
+    TimerConfigure(TIMER1_BASE, TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_PERIODIC);
+    TimerLoadSet(TIMER1_BASE, TIMER_A, SysCtlClockGet()/10000); //100us
+    IntRegister(INT_TIMER1A, ISRtimerDelay); //enlacem rutina interrrupcio
+    TimerIntEnable(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
+
+    //init SPI and GPIO pins
+    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOB))
+    {
+        SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+    }
+    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOC))
+    {
+        SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
+    }
+    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOA))
+    {
+        SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    }
+    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOE))
+    {
+        SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+    }
+    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_SSI2))
+    {
+        SysCtlPeripheralEnable(SYSCTL_PERIPH_SSI2);
+    }
+    GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, GPIO_PIN_3); //PA3 SPI_CS
+    GPIOPinTypeGPIOOutput(GPIO_PORTC_BASE, GPIO_PIN_6);  //PC6 nRF24 CE i PC5 int nRF24
+    GPIOPinTypeGPIOInput(GPIO_PORTC_BASE, GPIO_PIN_5); //a canviar (no fem servir)
+    GPIOIntRegister(GPIO_PORTC_BASE, nRF24ISR); // TODO // rutina interrupcio modul radio
+    GPIOIntTypeSet(GPIO_PORTC_BASE, GPIO_PIN_5, GPIO_FALLING_EDGE); //negedge interrupcio
+    GPIOIntEnable(GPIO_PORTC_BASE, GPIO_PIN_5); //habilitem ints de nRF24
+    IntEnable(INT_GPIOC);
+
+    nRF24L01_CE(false); //set CE to disable
+	nRF24L01_CSN(true); //SPI disable
+
+	GPIOPinConfigure(GPIO_PB4_SSI2CLK); //posar els pins q es fan servir
+    GPIOPinConfigure(GPIO_PB6_SSI2RX);
+    GPIOPinConfigure(GPIO_PB7_SSI2TX);
+    GPIOPinTypeSSI(GPIO_PORTB_BASE, GPIO_PIN_4 | GPIO_PIN_6 | GPIO_PIN_7); //posar pins SPI
+    SSIConfigSetExpClk(SSI2_BASE, SysCtlClockGet(), SSI_FRF_MOTO_MODE_0, SSI_MODE_MASTER, 5000000, 8); //5MHz
+    SSIEnable(SSI2_BASE);
+    // FEM servir SPI sense interrupcions
+}
+
+uint32_t TCount;
+void delay100us(uint32_t ticks)
+{
+    TCount=0;
+    TimerLoadSet(TIMER1_BASE, TIMER_A, SysCtlClockGet()/10000); //100us
+    IntEnable(INT_TIMER1A);
+    TimerEnable(TIMER1_BASE, TIMER_A);
+    while (TCount < ticks)
+    {
+    }
+    TimerDisable(TIMER1_BASE, TIMER_A);
+    IntDisable(INT_TIMER1A);
+}
+
+void ISRtimerDelay(void)
+{
+    TimerIntClear(TIMER1_BASE, TIMER_TIMA_TIMEOUT); //netejem flag
+    TCount++;
+}
+
+void SPI_send(uint8_t count, uint8_t * dataout, uint8_t * datain)
+{
+	bool rec = true;
+    if(datain==NULL){
+        rec=false;
+    }
+    uint8_t i,k,j;
+    j=0;
+    while(1){
+        i=0;
+        while(SSIDataPutNonBlocking(SSI2_BASE, *(dataout+i+j))) //omplim la TXFIFO
+        {
+            i++;
+            if(((i+j)>=count||(i>=8))){
+                break;
+            }
+        }
+        //s'ha omplert TX FIFO
+        //esperem a que es buidi
+        while((HWREG(SSI2_BASE + SSI_O_SR) & SSI_SR_BSY)); //esperem a que es buidi Tx
+        uint32_t var;
+        uint32_t *RxPointer;
+        for (k=0;k<i;k++){
+            if(rec){
+                RxPointer=(uint32_t*)&(*(datain+k+j));
+                SSIDataGetNonBlocking(SSI2_BASE, RxPointer);
+            } else{
+                SSIDataGetNonBlocking(SSI2_BASE, &var);
+            }
+        }
+        if((j+i)>=count){ //paraules enviades
+            break;
+        }
+        j=j+i; //si reiniciem bucle hem enviat 8 paraules
+    }
 }
